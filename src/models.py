e76ca55a2d716b3dd522d3805a1f367b8b9bbba2
wonjin0403 <37763659+wonjin0403@users.noreply.github.com>
2022-07-19 20:05:36 +0900

    Update models.py

--- src/models.py
+++ src/models.py
@@ -3,33 +3,41 @@ from utils import *
 
 #from pytorchcv.model_provider import get_model as ptcv_get_model
 import timm
-from torch import nn
 
 import math
 import torch
+from torch import nn
+
+import torch.nn as nn
+import numpy as np
+import torchvision.models as models
 from torch.nn import functional as F
 from torch.nn.parameter import Parameter
+from functools import partial
 
-class ArcMarginProduct(nn.Module):
-    def __init__(self, in_features, out_features):
+class ArcMarginProduct_subcenter(nn.Module):
+    def __init__(self, in_features, out_features, k=3):
         super().__init__()
-        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.weight = nn.Parameter(torch.FloatTensor(out_features*k, in_features))
         self.reset_parameters()
-
+        self.k = k
+        self.out_features = out_features
+        
     def reset_parameters(self):
-        nn.init.xavier_uniform_(self.weight)
-        # stdv = 1. / math.sqrt(self.weight.size(1))
-        # self.weight.data.uniform_(-stdv, stdv)
-
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        
     def forward(self, features):
-        cosine = F.linear(F.normalize(features), F.normalize(self.weight))
-        return cosine
+        cosine_all = F.linear(F.normalize(features), F.normalize(self.weight))
+        cosine_all = cosine_all.view(-1, self.out_features, self.k)
+        cosine, _ = torch.max(cosine_all, dim=2)
+        return cosine   
 
 def gem(x, p=3, eps=1e-6):
     return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
 
 class GeM(nn.Module):
-    def __init__(self, p=3, eps=1e-6, p_trainable=True):
+    def __init__(self, p=3, eps=1e-6, p_trainable=False):
         super(GeM,self).__init__()
         if p_trainable:
             self.p = Parameter(torch.ones(1)*p)
@@ -38,96 +46,161 @@ class GeM(nn.Module):
         self.eps = eps
 
     def forward(self, x):
-        return gem(x, p=self.p, eps=self.eps)       
+        ret = gem(x, p=self.p, eps=self.eps)   
+        return ret
     def __repr__(self):
         return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(self.eps) + ')'
 
     
-class Backbone(nn.Module):
 
-    
-    def __init__(self, name='resnet18', pretrained=True):
-        super(Backbone, self).__init__()
-        self.net = timm.create_model(name, pretrained=pretrained)
+class MultiAtrousModule(nn.Module):
+    def __init__(self, in_chans, out_chans, dilations):
+        super(MultiAtrousModule, self).__init__()
         
-        if 'regnet' in name:
-            self.out_features = self.net.head.fc.in_features
-        elif 'csp' in name:
-            self.out_features = self.net.head.fc.in_features
-        elif 'res' in name: #works also for resnest
-            self.out_features = self.net.fc.in_features
-        elif 'efficientnet' in name:
-            self.out_features = self.net.classifier.in_features
-        elif 'densenet' in name:
-            self.out_features = self.net.classifier.in_features
-        elif 'senet' in name:
-            self.out_features = self.net.fc.in_features
-        elif 'inception' in name:
-            self.out_features = self.net.last_linear.in_features
-
-        else:
-            self.out_features = self.net.classifier.in_features
-
-    def forward(self, x):
-        x = self.net.forward_features(x)
-
+        self.d0 = nn.Conv2d(in_chans, 256, kernel_size=3, dilation=dilations[0],padding='same')
+        self.d1 = nn.Conv2d(in_chans, 256, kernel_size=3, dilation=dilations[1],padding='same')
+        self.d2 = nn.Conv2d(in_chans, 256, kernel_size=3, dilation=dilations[2],padding='same')
+        self.conv1 = nn.Conv2d(256 * 3, out_chans, kernel_size=1)
+        self.relu = nn.ReLU()
+        
+    def forward(self,x):
+        
+        x0 = self.d0(x)
+        x1 = self.d1(x)
+        x2 = self.d2(x)
+        x = torch.cat([x0,x1,x2],dim=1)
+        x = self.conv1(x)
+        x = self.relu(x)
         return x
 
-    
+class SpatialAttention2d(nn.Module):
+    def __init__(self, in_c):
+        super(SpatialAttention2d, self).__init__()
+        self.conv1 = nn.Conv2d(in_c, 512, 1, 1)
+        self.bn = nn.BatchNorm2d(512)
+        self.act1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(512, 1, 1, 1)
+        self.softplus = nn.Softplus(beta=1, threshold=20) # use default setting.
+
+    def forward(self, x):
+        '''
+        x : spatial feature map. (b x c x w x h)
+        att : softplus attention score 
+        '''
+        x = self.conv1(x)
+        x = self.bn(x)
+        
+        feature_map_norm = F.normalize(x, p=2, dim=1)
+         
+        x = self.act1(x)
+        x = self.conv2(x)
+        att_score = self.softplus(x)
+        att = att_score.expand_as(feature_map_norm)
+        
+        x = att * feature_map_norm
+        return x, att_score   
+
+class OrthogonalFusion(nn.Module):
+    def __init__(self):
+        super(OrthogonalFusion, self).__init__()
+
+    def forward(self, fl, fg):
+
+        bs, c, w, h = fl.shape
+        
+        fl_dot_fg = torch.bmm(fg[:,None,:],fl.reshape(bs,c,-1))
+        fl_dot_fg = fl_dot_fg.reshape(bs,1,w,h)
+        fg_norm = torch.norm(fg, dim=1)
+        
+        fl_proj = (fl_dot_fg / fg_norm[:,None,None,None]) * fg[:,:,None,None]
+        fl_orth = fl - fl_proj
+        
+        f_fused = torch.cat([fl_orth,fg[:,:,None,None].repeat(1,1,w,h)],dim=1)
+        return f_fused  
+
+
 class Net(nn.Module):
-    def __init__(self, args, pretrained=True):
+    def __init__(self, cfg):
         super(Net, self).__init__()
+        self.cfg = cfg
+        self.n_classes = self.cfg["n_classes"]
+        self.backbone = timm.create_model("tf_mobilenetv3_large_100",#cfg["backbone"], 
+                                          pretrained=cfg["pretrained"], 
+                                          num_classes=0, 
+                                          global_pool="", 
+                                          in_chans=self.cfg["in_channels"], features_only = True)
+
         
-        self.args = args
-        self.backbone = Backbone(args.backbone, pretrained=pretrained)
+        if self.cfg["stride"] is not None:
+            self.backbone.conv_stem.stride = self.cfg["stride"]
+        backbone_out = self.backbone.feature_info[-1]['num_chs']
+        backbone_out_1 = self.backbone.feature_info[-2]['num_chs']
         
-        if args.pool == "gem":
-            self.global_pool = GeM(p_trainable=args.p_trainable)
-        elif args.pool == "identity":
+        feature_dim_l_g = 512 #1024
+        fusion_out = 2 * feature_dim_l_g
+
+        if cfg["pool"] == "gem":
+            self.global_pool = GeM(p_trainable=cfg["gem_p_trainable"])
+        elif cfg["pool"] == "identity":
             self.global_pool = torch.nn.Identity()
-        else:
+        elif cfg["pool"] == "avg":
             self.global_pool = nn.AdaptiveAvgPool2d(1)
 
-        self.embedding_size = args.embedding_size        
-        
-        # https://www.groundai.com/project/arcface-additive-angular-margin-loss-for-deep-face-recognition
-        if args.neck == "option-D":
-            self.neck = nn.Sequential(
-                nn.Linear(self.backbone.out_features, self.embedding_size, bias=True),
-                nn.BatchNorm1d(self.embedding_size),
-                torch.nn.PReLU()
-            )
-        elif args.neck == "option-F":
-            self.neck = nn.Sequential(
-                nn.Dropout(0.3),
-                nn.Linear(self.backbone.out_features, self.embedding_size, bias=True),
-                nn.BatchNorm1d(self.embedding_size),
-                torch.nn.PReLU()
-            )
-        else:
-            self.neck = nn.Sequential(
-                nn.Linear(self.backbone.out_features, self.embedding_size, bias=False),
-                nn.BatchNorm1d(self.embedding_size),
-            )
-            
-        self.head = ArcMarginProduct(self.embedding_size, args.n_classes)
-        
-        if args.pretrained_weights is not None:
-            self.load_state_dict(torch.load(args.pretrained_weights, map_location='cpu'), strict=False)
-            print('weights loaded from',args.pretrained_weights)
+        self.fusion_pool = nn.AdaptiveAvgPool2d(1)
+        self.embedding_size = cfg["embedding_size"]
 
+        self.neck = nn.Sequential(
+                nn.Linear(fusion_out, self.embedding_size, bias=True),
+                nn.BatchNorm1d(self.embedding_size),
+                torch.nn.PReLU()
+            )
+
+        if not self.cfg["headless"]:    
+            self.head_in_units = self.embedding_size
+            self.head = ArcMarginProduct_subcenter(self.embedding_size, self.n_classes)
+        #if self.cfg.loss == 'adaptive_arcface':
+        #    self.loss_fn = ArcFaceLossAdaptiveMargin(dataset.margins,self.n_classes,cfg.arcface_s)
+        #elif self.cfg.loss == 'arcface':
+        #    self.loss_fn = ArcFaceLoss(cfg.arcface_s,cfg.arcface_m)
+        #else:
+        #    pass
+        
+        self.mam = MultiAtrousModule(backbone_out_1, feature_dim_l_g, self.cfg["dilations"])
+        self.conv_g = nn.Conv2d(backbone_out, feature_dim_l_g, kernel_size=1)
+        self.bn_g = nn.BatchNorm2d(feature_dim_l_g, eps=0.001, momentum=0.1, affine=True, track_running_stats=True)
+        self.act_g =  nn.SiLU(inplace=True)
+        #self.attention2d = SpatialAttention2d(feature_dim_l_g)
+        self.fusion = OrthogonalFusion()
+        
+        
     def forward(self, input_dict, get_embeddings=False, get_attentions=False):
-
         x = input_dict['input']
         x = self.backbone(x)
         
-        x = self.global_pool(x)
-        x = x[:,:,0,0]
-        
-        x = self.neck(x)
+        x_l = x[-2]
+        x_g = x[-1]
 
-        logits = self.head(x)
+        x_l = self.mam(x_l)
+        #x_l, att_score = self.attention2d(x_l)
         
+        x_g = self.conv_g(x_g)
+        x_g = self.bn_g(x_g)
+        x_g = self.act_g(x_g)
+        
+        x_g = self.global_pool(x_g)
+        x_g = x_g[:,:,0,0]
+        
+        x_fused = self.fusion(x_l, x_g)
+        x_fused = self.fusion_pool(x_fused)
+        x_fused = x_fused[:,:,0,0]        
+        
+        x_emb = self.neck(x_fused)
+
+        if self.cfg["headless"]:
+            return x_emb
+
+        logits = self.head(x_emb)
+        #preds = logits.softmax(1)
         if get_embeddings:
             return {'logits': logits, 'embeddings': x}
         else:
